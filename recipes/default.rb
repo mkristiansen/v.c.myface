534ef8e66fe952dc1d2870e98f6b9fc33e3f5311
Morten Kristiansen <morten.kristiansen@gmail.com>
2014-01-23 10:47:43 +0000

    Synch

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,5 +6,25 @@
 # All rights reserved - Do Not Redistribute
 #
 
+yum_globalconfig '/etc/yum.conf.chef' do
+  action :delete
+end
+
+yum_globalconfig '/etc/yum.conf' do
+  cachedir '/var/cache/yum/$basearch/$releasever'
+  keepcache true
+  exclude 'kernel'	
+  debuglevel '2'
+  logfile '/var/log/yum.log'
+  exactarch=1
+  obsoletes=1
+  gpgcheck=1
+  plugins=1
+  installonly_limit=5
+  bugtracker_url 'http://bugs.centos.org/set_project.php?project_id=16&ref=http://bugs.centos.org/bug_report_page.php?category=yum'
+  distroverpkg 'centos-release'
+  action :create
+end
+
 include_recipe 'myface::webserver'
 include_recipe 'myface::database'
