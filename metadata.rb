4dd75fa7768bd7fc494419db70808161460ff026
Morten Kristiansen <morten.kristiansen@gmail.com>
2014-03-01 19:48:07 +0000

    Initial fileset

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,10 @@ description      'Installs/Configures myface'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.0'
 
+depends 'apt'
 depends 'mysql', '~> 4.0.0'
 depends 'apache2', '~> 1.8.0'
 depends 'database', '~> 1.6.0'
 depends 'yum', '~> 3.0.0'
+depends 'php', '~> 1.3.0'
+
